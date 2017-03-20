d5880ca8be5fe936d6680c463244169727cbc884
Michael Burman <yak@iki.fi>
2017-03-20 12:51:50 +0200

    Implement metricName queries

--- provider.go
+++ provider.go
@@ -8,11 +8,16 @@ import (
 	"bytes"
 
 	"github.com/hawkular/hawkular-client-go/metrics"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/client-go/pkg/api"
 	"k8s.io/custom-metrics-boilerplate/pkg/provider"
 	"k8s.io/metrics/pkg/apis/custom_metrics"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -117,13 +122,60 @@ If namespace is used and "metrics"" is the ResourceName, then name is the resour
 
 */
 
-func (h *HawkularProvider) GetRootScopedMetricByName(groupResource schema.GroupResource, name string, metricName string) (*custom_metrics.MetricValue, error) {
+func valueConverter(dp *metrics.Datapoint, t metrics.MetricType, unit string) (*resource.Quantity, error) {
+	var int64 val
+	// TODO Reoccurring theme.. this should be handled in the metrics client
+	switch t {
+	case metrics.Counter:
+		val = dp.Value.(int64)
+	case metrics.Gauge:
+		f := dp.Value.(float64)
+		val = int64(f) // We loose some precision, but that's acceptable in this case
+	}
+
+	// TODO Use unit (such as bytes) for the correct measurement scaling
+	return *resource.NewMilliQuantity(value*100, resource.DecimalSI), nil
+}
+
+func definitionToMetricValue(md *metrics.MetricDefinition, groupResource schema.GroupResource) (*custom_metrics.MetricValue, error) {
+	// Fetch async
+	dp, err := h.client.ReadRaw(md.Type, md.ID, metrics.LimitFilter(1), metrics.OrderFilter(metrics.DESC))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(dp) > 0 {
+		// Fetch async, cache the result
+		group, err := api.Registry.Group(groupResource.Group)
+		if err != nil {
+			return nil, err
+		}
+
+		kind, err := api.Registry.RESTMapper().KindFor(groupResource.WithVersion(group.GroupVersion.Version))
+		if err != nil {
+			return nil, err
+		}
+
+		val, err := valueConverter(dp[0], d.Type, d.Tags["units"])
+		if err != nil {
+			return nil, err
+		}
+
+		custom_metrics.MetricValue{
+			DescribedObject: api.ObjectReference{
+				APIVersion: groupResource.Group + "/" + runtime.APIVersionInternal,
+				Kind:       kind.Kind,
+				Name:       md.Tags[descriptorTag],
+				Namespace:  md.Tenant, // Or do we need the id instead of the name?
+			},
+			Timestamp: metav1.Time{dp[0].Timestamp},
+			Value:     val,
+		}
+	}
 
 }
 
-// GetRootScopedMetricBySelector fetches a particular metric for a set of root-scoped objects
-// matching the given label selector.
-func (h *HawkularProvider) GetRootScopedMetricBySelector(groupResource schema.GroupResource, selector labels.Selector, metricName string) (*custom_metrics.MetricValueList, error) {
+func tagsQueryBuilder(groupResource schema.GroupResource, selector labels.Selector, metricName string) metrics.TagsQueryFilter {
 	var buffer bytes.Buffer
 	buffer.WriteString(fmt.Sprintf("%s = %s AND %s = %s", descriptorTag, metricName, resourceTag, groupResource.Resource))
 
@@ -132,27 +184,70 @@ func (h *HawkularProvider) GetRootScopedMetricBySelector(groupResource schema.Gr
 		buffer.WriteString(h.labelSelectorToHawkularTagsQuery(selector))
 	}
 
-	metrics.TagsQueryFilter(buffer.String())
+	return metrics.TagsQueryFilter(buffer.String())
+}
+
+func (h *HawkularProvider) GetRootScopedMetricByName(groupResource schema.GroupResource, name string, metricName string) (*custom_metrics.MetricValue, error) {
+
+}
+
+// GetRootScopedMetricBySelector fetches a particular metric for a set of root-scoped objects
+// matching the given label selector.
+func (h *HawkularProvider) GetRootScopedMetricBySelector(groupResource schema.GroupResource, selector labels.Selector, metricName string) (*custom_metrics.MetricValueList, error) {
+	// We fetch definitions first to be able to do manual label selection for procedures we don't support
+	defs, err := h.client.Definitions(tagsQueryBuilder(groupResource, selector, metricName))
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO Now filter here if there was some integer label selectors..
+
+	// TODO Modify this to fetch all the metrics with a single call to Hawkular-Metrics once we know the real split between rate selection & gauge selection
+	// We can request rates for each metric if we wanted, but when is it necessary? There's no specifications or metadata to make that selection.
+	// For example, are counter types always with rate and gauge with always exact? There are situations where "gauges" can actually give some rate, such as
+	// filesystem usage growth rate (while filesystem usage is actually a gauge metric)
+
+	mv := make([]custom_metrics.MetricValue, 0, len(defs))
+
+	// Fetch the requested values
+	// TODO Turn into its own function to allow reuse
+	for _, d := range defs {
+		mv = append(mv, definitionToMetricValue(d, groupResource))
+	}
+
+	return &custom_metrics.MetricValueList{
+		Items: mv,
+	}, nil
 }
 
 // GetNamespacedMetricByName fetches a particular metric for a particular namespaced object.
 func (h *HawkularProvider) GetNamespacedMetricByName(groupResource schema.GroupResource, namespace string, name string, metricName string) (*custom_metrics.MetricValue, error) {
 	// TODO How are we going to select the metricName here properly? How are we storing the metric?
-
-	// The devil is in the GroupResource
-
-	groupResource.Group    // "router"
-	groupResource.Resource // "Such as filesystem/usage" ?
+	// if name is * we do something and if "metrics" something else.. or ?
 }
 
 // GetNamespacedMetricBySelector fetches a particular metric for a set of namespaced objects
 // matching the given label selector.
 func (h *HawkularProvider) GetNamespacedMetricBySelector(groupResource schema.GroupResource, namespace string, selector labels.Selector, metricName string) (*custom_metrics.MetricValueList, error) {
-	tenantModifier := metrics.Tenant(namespace)
-	_ = labelSelectorToHawkularTagsQuery(selector)
+	// We fetch definitions first to be able to do manual label selection for procedures we don't support
+	defs, err := h.client.Definitions(metrics.Tenant(namespace), tagsQueryBuilder(groupResource, selector, metricName))
+	if err != nil {
+		return nil, err
+	}
 
-	// I should do readBuckets and get it all..
-	h.client.Definitions(tenantModifier)
+	// TODO Refactor following to reusable function (copy from other methods) as soon as name versioned ones are implemented
+
+	mv := make([]custom_metrics.MetricValue, 0, len(defs))
+
+	// Fetch the requested values
+	// TODO Turn into its own function to allow reuse
+	for _, d := range defs {
+		mv = append(mv, definitionToMetricValue(d, groupResource))
+	}
+
+	return &custom_metrics.MetricValueList{
+		Items: mv,
+	}, nil
 }
 
 // ListAllMetrics provides a list of all available metrics at
