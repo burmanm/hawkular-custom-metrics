c37ac33dc76b8cc6dcf0326bf06c0f9d5f3c656a
Michael Burman <yak@iki.fi>
2017-03-21 09:18:34 +0200

    Work on the init.go on different computer

---
+++ init.go
@@ -0,0 +1,132 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hawkular
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apiserver/pkg/util/logs"
+	"k8s.io/custom-metrics-boilerplate/pkg/sample-cmd/server"
+
+	"github.com/spf13/cobra"
+	coreclient "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/custom-metrics-boilerplate/pkg/cmd/server"
+	"k8s.io/custom-metrics-boilerplate/pkg/sample-cmd/provider"
+)
+
+// NewCommandStartMaster provides a CLI handler for 'start master' command
+func NewCommandStartAdapterServer(out, errOut io.Writer, stopCh <-chan struct{}) *cobra.Command {
+	baseOpts := server.NewCustomMetricsAdapterServerOptions(out, errOut)
+	o := SampleAdapterServerOptions{
+		CustomMetricsAdapterServerOptions: baseOpts,
+	}
+
+	cmd := &cobra.Command{
+		Short: "Launch the Hawkular custom metrics API adapter server",
+		Long:  "Launch the Hawkular custom metrics API adapter server",
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := o.Complete(); err != nil {
+				return err
+			}
+			if err := o.Validate(args); err != nil {
+				return err
+			}
+			if err := o.RunCustomMetricsAdapterServer(stopCh); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	o.SecureServing.AddFlags(flags)
+	o.Authentication.AddFlags(flags)
+	o.Authorization.AddFlags(flags)
+	o.Features.AddFlags(flags)
+
+	flags.StringVar(&o.RemoteKubeConfigFile, "lister-kubeconfig", o.RemoteKubeConfigFile, ""+
+		"kubeconfig file pointing at the 'core' kubernetes server with enough rights to list "+
+		"any described objets")
+
+	flags.StringVar(&o.HawkularConfigUri, "hawkular-uri", o.HawkularConfigUri,
+		"Hawkular configuration in a URL format")
+
+	return cmd
+}
+
+func (o SampleAdapterServerOptions) RunCustomMetricsAdapterServer(stopCh <-chan struct{}) error {
+	config, err := o.Config()
+	if err != nil {
+		return err
+	}
+
+	var clientConfig *rest.Config
+	if len(o.RemoteKubeConfigFile) > 0 {
+		loadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: o.RemoteKubeConfigFile}
+		loader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &clientcmd.ConfigOverrides{})
+
+		clientConfig, err = loader.ClientConfig()
+	} else {
+		clientConfig, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		return fmt.Errorf("unable to construct lister client config to initialize provider: %v", err)
+	}
+
+	client, err := coreclient.NewForConfig(clientConfig)
+	if err != nil {
+		return fmt.Errorf("unable to construct lister client to initialize provider: %v", err)
+	}
+
+	cmProvider := provider.NewFakeProvider(client)
+
+	server, err := config.Complete().New(cmProvider)
+	if err != nil {
+		return err
+	}
+	return server.GenericAPIServer.PrepareRun().Run(stopCh)
+}
+
+type SampleAdapterServerOptions struct {
+	*server.CustomMetricsAdapterServerOptions
+
+	// RemoteKubeConfigFile is the config used to list pods from the master API server
+	RemoteKubeConfigFile string
+	HawkularConfigUri    string
+}
+
+func main() {
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if len(os.Getenv("GOMAXPROCS")) == 0 {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	cmd := server.NewCommandStartAdapterServer(os.Stdout, os.Stderr, wait.NeverStop)
+	cmd.Flags().AddGoFlagSet(flag.CommandLine)
+	if err := cmd.Execute(); err != nil {
+		panic(err)
+	}
+}
